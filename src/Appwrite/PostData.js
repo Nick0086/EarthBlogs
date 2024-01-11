585e6cd752631f8d847afe9d862747fb38a52cc5
Nick0086 <nikunjkalathiya86@gmail.com>
2024-01-11 15:40:47 +0530

    addpost successfull

---
+++ src/Appwrite/PostData.js
@@ -0,0 +1,123 @@
+import { Client, Databases, ID, Storage } from "appwrite";
+import conf from "../conf/conf";
+
+export class PostService {
+
+    client = new Client;
+    databases;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.databases = new Databases(this.client);
+        this.storage = new Storage(this.client)
+    }
+
+    async createPost({ Title, Featureimage, Category, status, Content, userId, PostId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaaseId,
+                conf.appwriteCollectionId,
+                PostId,
+                {
+                    Title,
+                    Featureimage,
+                    Category,
+                    status,
+                    Content,
+                    userId,
+                    PostId
+                }
+            )
+        } catch (error) {
+            console.error("Appwrite serive :: createPost :: error", error);
+            return false;
+        }
+    }
+    async updatePost({Title, Featureimage, Category, status, Content,PostId}){
+        try {
+
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaaseId,
+                conf.appwriteCollectionId,
+                PostId,
+                {
+                    Title,
+                    Featureimage,
+                    Category,
+                    status,
+                    Content,
+                }
+            )
+            
+        } catch (error) {
+            console.error("Appwrite serive :: updatePost :: error", error);
+        }
+    }
+    async getPost(PostId){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaaseId,
+                conf.appwriteCollectionId,
+                PostId
+            )
+        } catch (error) {
+            console.error("Appwrite serive :: getPost :: error", error);
+        }
+    }
+    async delatePost(PostId){
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwriteDatabaaseId,
+                conf.appwriteCollectionId,
+                PostId
+            )
+        } catch (error) {
+            console.error("Appwrite serive :: delatePost :: error", error);
+        }
+    }
+
+    // ================== File Service =============
+
+    async uploadFile(file){
+        try {
+            return await this.storage.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file,
+            )   
+        } catch (error) {
+            console.error("Appwrite serive :: uploadFile :: error", error);
+            return false;
+        }
+    }
+
+    async delatePost(fileId){
+        try {
+            return await this.storage.deleteFile(
+                conf.appwriteBucketId,
+                fileId,
+            )
+        } catch (error) {
+            console.error("Appwrite serive :: delatePost :: error", error);   
+        }
+    }
+
+    async getFilePreview(fileId){
+        try {
+            return await this.storage.getFilePreview(
+                conf.appwriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            console.error("Appwrite serive :: getFilePreview :: error", error); 
+        }
+    }
+
+}
+
+const postService = new PostService();
+export default postService;
--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import Input from '../input';
 import Button from '../Button';
@@ -6,18 +6,34 @@ import RTE from '../RTE/RTE';
 import Selector from '../Selector';
 import './PostForm.css';
 import { ToastContainer, toast } from 'react-toastify';
-
-
-
+import { useSelector } from 'react-redux';
+import postService from '../../Appwrite/PostData';
+import { useNavigate } from 'react-router-dom';
+import Spinner from "../Spinner/Spinner"
 
 function PostForm() {
     const { register, handleSubmit, control, formState: { errors } } = useForm();
-
+    const userData = useSelector((state) => state.auth.userData)
+    const navigate = useNavigate();
+    const [loading, SetLoading] = useState(true);
 
     const postHandler = async (data) => {
-        console.log(data);
-    }
 
+        SetLoading(false);
+        const imageFile = await postService.uploadFile(data.image[0]);
+        const fileId = imageFile.$id;
+
+        data.Featureimage = fileId;
+        data.PostId = fileId.concat(imageFile.signature).slice(0, 30);
+        data.userId = userData.$id;
+
+        const addPost = await postService.createPost(data);
+
+        if (addPost) {
+            navigate('/');
+        }
+        SetLoading(true);
+    };
 
     // fuction for send notification error in post data fill
     const notify = () => {
@@ -40,70 +56,77 @@ function PostForm() {
     }, [errors]);
 
     return (
-        <div className='bg-gray-100 post-bg' >
-            <ToastContainer />
-            <div className='lg:py-36 md:py-12 py-10 container' >
-                <form className=' lg:w-3/4 bg-white bg-opacity-60 shadow-xl backdrop-blur-sm   md:w-[90%] w-[95%] rounded-2xl mx-auto md:p-8 p-4' onSubmit={handleSubmit(postHandler)} >
-                    <h3 className='md:text-2xl text-xl font-medium text-dark-green text-center mb-10'>Create New Post</h3>
-                    <div>
-                        <Input
-                            label="Title"
-                            labelclass="text-[16px]"
-                            type="text"
-                            placeholder="Title"
-                            {...register("Title",
-                                {
-                                    required: "Title is required",
-                                    maxLength: {
-                                        value: 120,
-                                        message: 'Title cannot exceed 120 characters',
-                                    }
+        <>
+            {
+                loading ? <div className='bg-gray-100 post-bg' >
+                    <ToastContainer />
+                    <div className='lg:py-36 md:py-12 py-10 container' >
+                        <form className=' lg:w-3/4 bg-white bg-opacity-60 shadow-xl backdrop-blur-sm   md:w-[90%] w-[95%] rounded-2xl mx-auto md:p-8 p-4' onSubmit={handleSubmit(postHandler)} >
+                            <h3 className='md:text-2xl text-xl font-medium text-dark-green text-center mb-10'>Create New Post</h3>
+                            <div>
+                                <Input
+                                    label="Title"
+                                    labelclass="text-[16px]"
+                                    type="text"
+                                    placeholder="Title"
+                                    {...register("Title",
+                                        {
+                                            required: "Title is required",
+                                            maxLength: {
+                                                value: 120,
+                                                message: 'Title cannot exceed 120 characters',
+                                            }
+                                        })}
+                                />
+                                <RTE
+                                    label="Content :"
+                                    labelclass="text-[16px]"
+                                    name="Content"
+                                    control={control}
+                                    {...register("Content",
+                                        {
+                                            required: "Content is required",
+                                            maxLength: {
+                                                value: 3000,
+                                                message: 'Title cannot exceed 3000 characters',
+                                            }
+                                        })}
+                                />
+                                <Input
+                                    label="Featured Image"
+                                    labelclass="text-[16px]"
+                                    placeholder="Featured Image"
+                                    type="file"
+                                    classname="border-b-0"
+                                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                                    {...register("image", { required: "Post Thumbnail is required", 
                                 })}
-                        />
-                        <RTE
-                            label="Content :"
-                            labelclass="text-[16px]"
-                            name="Content"
-                            control={control}
-                            {...register("Content",
-                                {
-                                    required: "Content is required",
-                                    maxLength: {
-                                        value: 3000,
-                                        message: 'Title cannot exceed 3000 characters',
-                                    }
-                                })}
-                        />
-                        <Input
-                            label="Featured Image"
-                            labelclass="text-[16px]"
-                            placeholder="Featured Image"
-                            type="file"
-                            classname="border-b-0"
-                            accept="image/png, image/jpg, image/jpeg, image/gif"
-                            {...register("image", { required: "Post Thumbnail is required", })}
-                        />
-                        <Selector
-                            labelclass="text-[16px]"
-                            label="Status"
-                            options={["Active", "Inactive"]}
-                            className="mb-6"
-                            {...register("status", { required: true })}
-                        />
-                        <Selector
-                            label="Category"
-                            labelclass="text-[16px]"
-                            options={["Personal", "News", "Sport", "Travel", "Food", "Fashion", "Finance", "Music", "Business", "Lifesyle"]}
-                            className="mb-6 "
-                            {...register("Category", { required: true })}
-                        />
-                        <div className='text-center mt-6'>
-                            <Button type='submit'>Add Post</Button>
-                        </div>
+                                />
+                                <Selector
+                                    labelclass="text-[16px]"
+                                    label="Status"
+                                    options={["Active", "Inactive"]}
+                                    className="mb-6"
+                                    {...register("status", { required: true })}
+                                />
+                                <Selector
+                                    label="Category"
+                                    labelclass="text-[16px]"
+                                    options={["Personal", "News", "Sport", "Travel", "Food", "Fashion", "Finance", "Music", "Business", "Lifesyle"]}
+                                    className="mb-6 "
+                                    {...register("Category", { required: true })}
+                                />
+                                <div className='text-center mt-6'>
+                                    <Button type='submit'>Add Post</Button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
-        </div>
+                </div> :
+                <Spinner/>
+            }
+        </>
+
     )
 }
 
