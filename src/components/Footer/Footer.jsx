2f6a03e9090df327c51951fa67fe70e128c129ba
Nick0086 <nikunjkalathiya86@gmail.com>
2024-01-24 18:22:55 +0530

    footer complate

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Logo from "../../assets/Logo.png"
 function Footer() {
+
   return (
-    <div className='my-20' >Footer</div>
+    <div className='container py-9'>
+      <div className='flex justify-between items-center' >
+        <div className='basis-1/3' >
+          <img src={Logo} className='md:w-[70%] w-[90%] ' alt="" srcset="" />
+        </div>
+        <div className='basis-2/3' >
+          <p className='md:text-sm text-xs tracking-tight text-end font-semibold ' >©2022 Earth. All right reserved.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
