72eea088ecd54b8f11481b79be8356e6e330fbc5
Nick0086 <nikunjkalathiya86@gmail.com>
2024-01-26 15:31:17 +0530

    ui update

--- src/components/AdminPostCard/AdminPostCard.jsx
+++ src/components/AdminPostCard/AdminPostCard.jsx
@@ -3,6 +3,7 @@ import postService from '../../Appwrite/PostData';
 import Button from '../Button';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../Spinner/Spinner';
+import TimeAgo from '../TimeAgo/TimeAgo';
 
 function AdminPostCard({ data, getpost }) {
 
@@ -43,7 +44,8 @@ function AdminPostCard({ data, getpost }) {
                     </div>
                     <div className='md:col-span-7 col-span-full' >
                         <h3 className='font-semibold text-lg mb-2' >{(data.Title).substring(0, 50) + "..."}</h3>
-                        <p className='text-sm font-medium tracking-tighter' >{data.$createdAt.split('T')[0].split('-')[2]} - {data.$createdAt.split('T')[0].split('-')[1]} - {data.$createdAt.split('T')[0].split('-')[0]}</p>
+                        <p className='text-sm font-medium tracking-tighter mb-1' >{data.$createdAt.split('T')[0].split('-')[2]} - {data.$createdAt.split('T')[0].split('-')[1]} - {data.$createdAt.split('T')[0].split('-')[0]}</p>
+                        <p className='text-sm font-medium tracking-tight' >{data.View ? `${data.View} views` : "No View"}</p>
                     </div>
                     <div className='md:col-span-2 col-span-full  flex flex-row  md:flex-col items-center md:justify-between justify-center gap-2' >
                         <Button classname='md:w-[70px] md:p-2 bg-blue-600 hover:bg-blue-900 rounded-lg text-sm' onClick={() => navigate(`/editpost/${data.$id}`)} >Edit</Button>
--- src/components/DetailPostCard/DetailPostCard.jsx
+++ src/components/DetailPostCard/DetailPostCard.jsx
@@ -150,19 +150,19 @@ function DetailPostCard({ post }) {
                  {/* code for comment on post    */}
                 <div className='my-5 py-4 border-t-2 border-opacity-50 border-green-700' >
                     <h3 className='text-3xl font-bold mb-3' >Comments <span className='text-2xl font-medium' >({allcomments && allcomments.length})</span></h3>
-                    <form className='grid grid-cols-12 gap-x-2' onSubmit={commentHandler} >
+                    <form className='grid grid-cols-12 md:gap-x-2 gap-x-1' onSubmit={commentHandler} >
                         <Input
                             placeholder="Enter Your Comment..."
-                            classname="bg-white shadow-md rounded-xl px-2 col-span-10 mb-[0px] "
+                            classname="bg-white shadow-md rounded-xl px-2 lg:col-span-10 col-span-9 mb-[0px] "
                             value={Comment}
                             onChange={(e) => setComment(e.target.value)}
                         />
                         <Button
-                            classname="md:w-[auto] rounded-xl col-span-2"
+                            classname="md:w-[auto] w-[auto] rounded-xl lg:col-span-2 col-span-3 text-xs tracking-tight px-[2px]"
                             type='submit'
                         >Comment</Button>
                     </form>
-                    <div className='bg-white rounded-lg overflow-auto custom-scrollbar comment-scroll mt-2 shadow-md h-screen ' >
+                    <div className='bg-white rounded-lg overflow-auto custom-scrollbar comment-scroll mt-2 shadow-md max-h-screen ' >
                         {
                             allcomments && allcomments.map((data, index) => (
                                 <div key={index} className='p-3 my-1 last:border-b-0 border-b-[1px] border-gray-600' >
